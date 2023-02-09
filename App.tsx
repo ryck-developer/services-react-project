e20e1149e62ec944c0f7feed01b4c4f64dbca264
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-02-09 15:21:22 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Luiz Henrique Chaves</h1>
+      <p>app sendo desenvolvido em Reatjs</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# services-react-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ahqraw)
